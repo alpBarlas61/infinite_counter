fe45cc579008567e7cc342e5d2a6631274429221
alpBarlas61 <88983256+alpBarlas61@users.noreply.github.com>
2022-01-09 23:15:14 +0300

    Add files via upload

---
+++ counter_infinite.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	int loop =1,number;
+	while(loop=1)
+	{
+		number +=1;
+		printf("%d \n",number);
+		getch();
+	}
+	
+	return 0;
+}
